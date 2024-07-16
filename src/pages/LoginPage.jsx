0447f842f1ea368a29c2c6c5750108dc85bdf119
Danne Uriel Boiser <danne.boiser@ciit.edu.ph>
2024-07-16 23:25:26 +0800

    Created LoginPage and SignupPage page components.

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+/**
+ *  TODO:   Create the Login Page here.
+ *          You may use any assets you see fit for this page component.
+ */
+export default function LoginPage() {
+    return (
+        <>
+
+        </>
+    );
+}
---
+++ src/pages/SignupPage.jsx
@@ -0,0 +1,11 @@
+/**
+ *  TODO:   Create the Sign Up Page here.
+ *          You may use any assets you see fit for this page component.
+ */
+export default function SignupPage() {
+    return (
+        <>
+
+        </>
+    );
+}
