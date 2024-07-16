bfa4aadb640cb776b3eff669b5eed9f2ec5e4f45
ciit-kojiescolar <129504627+ciit-kojiescolar@users.noreply.github.com>
2024-07-16 23:50:43 +0800

    implemented login page
    With css design and no firebase implementation yet.

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,10 +2,44 @@
  *  TODO:   Create the Login Page here.
  *          You may use any assets you see fit for this page component.
  */
-export default function LoginPage() {
-    return (
-        <>
 
-        </>
-    );
+import '../css/LoginPages.css';
+import React, {useState} from 'react';
+
+
+export default function LoginPage() {
+
+    const[email, setEmail] = useState ('');
+    const[password, setPassword] = useState('');
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        console.log ('Email', email);
+        console.log ('Password', password)
+    };
+
+    return (
+    <div className='tomacus-container'>
+        <h1>TOMACUS</h1>
+        <h2>Login</h2>
+        <form onSubmit={handleSubmit}>
+         <div className='input-group'>
+            <label>Email</label>
+            <input type='email' value={email} onChange={(event) => setEmail(event.target.value)} required/>
+            </div>
+
+            <div className='input-group'>
+                <label>Password</label>
+                <input type='password' value={password} onChange={(event) => setPassword(event.target.value)} required/>  
+            </div>
+            <button type="submit">Login</button>
+        </form>
+        <p>
+            Don't have an account? <a href='/register'>Create one!</a>
+        </p>
+    </div>
+  );
+    
 }
