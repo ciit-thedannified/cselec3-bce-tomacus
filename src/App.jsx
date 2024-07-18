aee3f63062eef2a2bac609bb9f8679e6ca1336b5
Danne Uriel Boiser <danne.boiser@ciit.edu.ph>
2024-07-18 15:24:06 +0800

    Implemented BrowserRouter component in main files.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import LoginPage from "./pages/LoginPage.jsx";
+import "./index.css"
 
 function App() {
-  return (
-      <LoginPage />
-  );
+    return (
+        <LoginPage />
+    )
 }
 
 export default App
