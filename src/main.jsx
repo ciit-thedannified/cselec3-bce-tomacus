b494d6ae99bac6b2347a40663b19533941caf77b
Danne Uriel Boiser <danne.boiser@ciit.edu.ph>
2024-07-20 11:00:12 +0800

    Adjusted main files

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AppRoutes from "./routes/AppRoutes.jsx";
 import {RouterProvider} from "react-router-dom";
+import {UserProvider} from "./contexts/UserContext.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={AppRoutes} />
+      <UserProvider>
+          <RouterProvider router={AppRoutes} />
+      </UserProvider>
   </React.StrictMode>,
 )
