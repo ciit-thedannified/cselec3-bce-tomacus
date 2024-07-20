ad4896894548c494a27b4ea69afe0e383d202a3f
Danne Uriel Boiser <danne.boiser@ciit.edu.ph>
2024-07-20 10:59:50 +0800

    Adjusted DashboardPage files

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -5,11 +5,58 @@
  *          b)  Sidebar, containing all Pomodoro schedules of the user
  *          c)  Interface, displaying interactions in the app
  */
-import {Outlet} from "react-router-dom";
+import {Link, Outlet, useNavigate} from "react-router-dom";
 import '../css/DashboardPage.css'
 import ScheduleCard from "../components/ScheduleCard";
+import {UserAuth} from "../contexts/UserContext.jsx";
+import {useEffect, useState} from "react";
+import {collection, query, where, onSnapshot} from "firebase/firestore";
+import {Database} from "../firebase/firebase.config.js";
+import {addPomodoroSchedule} from "../services/ScheduleServices.js";
 
 export default function DashboardPage() {
+
+    const {currentUser, username, logoutUser} = UserAuth();
+    const [schedules, setSchedules] = useState([]);
+    const navigate = useNavigate();
+
+    async function handleAddSchedule(event) {
+        event.preventDefault();
+
+        navigate(`/dashboard/schedule/new`);
+    }
+
+    async function handleLogout(event) {
+        event.preventDefault();
+
+        try {
+            await logoutUser()
+                .then(() => {
+                    navigate('/');
+                });
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    useEffect(() => {
+        if (!currentUser)
+            navigate('/')
+        else {
+            const col = collection(Database, 'schedules');
+
+            const q = query(col, where("owner", "==", currentUser.uid));
+
+            onSnapshot(q, (snapshot) => {
+                let res = []
+                snapshot.forEach(doc => {
+                    res.push({id: doc.id, el: <ScheduleCard ID={doc.id}/>});
+                });
+                setSchedules(res);
+            })
+        }
+    }, [currentUser]);
+
     return (
         <div className="main container-fluid">
             <div className="row">
@@ -19,9 +66,9 @@ export default function DashboardPage() {
                         <img className="tomato-logo" src="src/assets/tomato4.png"></img>
                         <div className="logo"> TOMACUS</div>
                         <div className="menu">
-                            <span>SCHEDULES</span>
-                            <span>PROFILE</span>
-                            <span>HELLO, {`{USER}`}</span>
+                            <span><Link to="/dashboard">SCHEDULES</Link></span>
+                            <span><Link to="/dashboard/profile">PROFILE</Link></span>
+                            <span onClick={handleLogout}>LOGOUT, {username}</span>
                         </div>
                     </div>
                 </div>
@@ -34,24 +81,24 @@ export default function DashboardPage() {
                         <div className="pomodoro-list">
                             <div className="pomodoro-header">
                                 <span>Pomodoro List</span>
-                                <button className="add-btn">+</button>
+                                <button onClick={handleAddSchedule} className="add-btn">+</button>
                             </div>
-                            <div className="pomodoro-item">
-                                <ScheduleCard/>
-                            </div>
-                            <div className="pomodoro-item">
-                                <ScheduleCard/>
-                            </div>
-                            <div className="pomodoro-item">
-                                <ScheduleCard/>
-                            </div>               
+                            {
+                                schedules.map(
+                                    scheduleObject => (
+                                        <div key={scheduleObject.id} className="pomodoro-item">
+                                            {scheduleObject.el}
+                                        </div>
+                                    )
+                                )
+                            }
                         </div>
                     </div>
                 </div>
 
                 <div className="col-8">
                     {/* [SECTION] Interface, outlet */}
-                        <Outlet />
+                    <Outlet/>
                 </div>
             </div>
         </div>
